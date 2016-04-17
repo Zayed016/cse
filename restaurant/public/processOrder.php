773e55f48ed0c0eb9be01153d4971404b97dfc5a
Zayed016 <zayedvoices@gmail.com>
2016-04-17 21:59:02 +0600

    database seed

---
+++ restaurant/public/processOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+$n = $_POST['hidName'];
+$t = $_POST['hidType'];
+$p = $_POST['hidPrice'];
+
+echo "Name: " . $n . "<br>";
+echo "Type: " . $t . "<br>";
+echo "Price: " . $p . "<br>";
+
+?>
---
+++ restaurant/resources/views/menubar.blade.php
@@ -0,0 +1,64 @@
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+        <link href="css/datepicker.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker.min.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker.standalone.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker.standalone.min.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker3.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker3.min.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker3.standalone.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker3.standalone.min.css" rel="stylesheet">
+        <link href="css/bootstrap-datetimepicker.css" rel="stylesheet">
+        
+        
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap-datepicker.js"></script>
+        <script type="text/javascript" src="js/npm.js"></script>
+        <script type="text/javascript" src="js/bootstrap-datepicker.min.js"></script>
+    <script type="text/javascript" src="js/moment.js"></script>
+    <script type="text/javascript" src="js/transition.js"></script>
+    <script type="text/javascript" src="js/collapse.js"></script>
+    <script type="text/javascript" src="js/bootstrap-datetimepicker.js"></script>
+    <script type="text/javascript" src="js/moment-with-locales.js"></script>
+
+<nav  style="padding: 10px 10px 10px 30px; "class="navbar navbar-inverse navbar-fixed-top">
+<div class="container-fluid">
+
+
+    
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Restaurant</a>
+    </div>
+
+    
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul style= "font-size:28px;"class="nav navbar-nav">
+        
+         <li><a href="home">Home</a></li>
+        <li><a href="menu">Menu</a></li>
+        <li><a href="reservation">Table book</a></li>
+      	<li><a href="#">About Us</a></li>
+      	<li><a href="#">Contact Us</a></li>
+      </ul>
+      <div style="float: right"><form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      </div>
+      </div>
+      
+</div>
+</nav>
